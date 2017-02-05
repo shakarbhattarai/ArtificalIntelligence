c3f0938861db3006b8f8799176acf8bea0f5d918
Shakar Bhattarai <shakarbhattarainp@gmail.com>
2017-02-05 14:28:34 +0545

    Add files via upload

---
+++ 2nd.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import quandl as Quandl,math, datetime
+import numpy as np
+from sklearn import preprocessing, cross_validation, svm
+from sklearn import tree
+df=pd.read_csv("red.csv",parse_dates=True,sep=';')
+X=np.array(df.drop(['quality'],1))
+y=np.array(df['quality'])
+print df.tail()
+X_train,X_test,y_train,y_test=cross_validation.train_test_split(X,y,test_size=0.1)
+clf=tree.DecisionTreeClassifier()
+clf.fit(X_train,y_train)
+accuracy=clf.score(X_test,y_test)
+
+df['forecast']=clf.predict(X)
+df['error']=df['quality']-df['forecast']
+
+df['error'].plot()
+
+
